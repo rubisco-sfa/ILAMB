fixes to latent heat, phase computation borken for gridded sets

src/ILAMB/LEFluxnetSites.py
@@ -3,6 +3,7 @@ import matplotlib.colors as colors
 from Variable import Variable,FromNetCDF4
 from netCDF4 import Dataset
 from Post import ColorBar,TaylorDiagram
+from ilamblib import VarNotOnTimeScale
 import pylab as plt
 import numpy as np
 import os,glob
@@ -99,12 +100,16 @@ class LEFluxnetSites():
                                      output_unit="J m-2 s-1")
         cdata["le"] = mod_le
         
-        # write confrontation result file
-        f = Dataset("%s/%s_%s.nc" % (self.output_path,self.name,m.name),mode="w")
-        
         # the observational site data has many holes, mask the model
         # data so we are comparing apples to apples
-        mod_le.data.mask += obs_le.data.mask
+        try:
+            mod_le.data.mask += obs_le.data.mask
+        except:
+            raise VarNotOnTimeScale("Model has %d time points and observations have %d" % (mod_le.data.shape[0],obs_le.data.shape[0]))
+        
+        # write confrontation result file
+        f = Dataset("%s/%s_%s.nc" % (self.output_path,self.name,m.name),mode="w")
+        f.setncatts({"name":m.name,"color":m.color})
         
         # integrate over the time period
         obs_le_timeint = obs_le.integrateInTime(mean=True).convert("J m-2 s-1")
@@ -199,11 +204,15 @@ class LEFluxnetSites():
         maxLE   = timeint["Benchmark"].data.max()
         maxBias = 0.
         models  = []
+        clr     = []
         cor     = []
         std     = []
         for fname in glob.glob(pattern):
-            mname = fname[:-3].split("_")[-1]
+            f     = Dataset(fname)
+            mname = f.getncattr("name")
             models.append(mname)
+            clr.append(f.getncattr("color"))
+            f.close()
             timeint[mname] = FromNetCDF4(fname,"hfls_integrated_over_time_and_divided_by_time_period")
             bias   [mname] = timeint[mname].data-timeint["Benchmark"].data
             minLE   = min(minLE  ,timeint[mname].data.min())
@@ -265,9 +274,10 @@ class LEFluxnetSites():
         fig.savefig("%s/legend_bias.png" % (self.output_path))
         plt.close()
 
+        # spatial variation
         fig = plt.figure(figsize=(6.8,6.8))
         TaylorDiagram(np.asarray(std),np.asarray(cor),1.0,
-                      fig,['k']*len(std),normalize=False)
+                      fig,clr,normalize=False)
         fig.savefig("%s/spatial_variance.png" % (self.output_path))
         plt.close()
             

src/ILAMB/Variable.py
@@ -400,7 +400,8 @@ class Variable:
             dt[0]     = 0.5*(self.time[1] -self.time[0])  + self.time[0]-t0
             dt[-1]    = 0.5*(self.time[-1]-self.time[-2]) + tf-self.time[-1]
             dt       *= (self.time>=t0)*(self.time<=tf)
-            dt        = (dt[:,np.newaxis]*(self.data.mask==0)).sum(axis=0)
+            for i in range(self.data.ndim-1): dt = np.expand_dims(dt,axis=-1)
+            dt        = (dt*(self.data.mask==0)).sum(axis=0)
             integral /= dt
             unit     += " d-1"
             name     += "_and_divided_by_time_period"
@@ -543,9 +544,12 @@ class Variable:
         vmean,vstd,tmax,tmaxstd = il.AnnualCycleInformation(var.time,var.data)
         return Variable(vmean,var.unit,name="annual_cycle_of_%s" % self.name,std=vstd,time=mid_months),tmax,tmaxstd
         
-    def phase(self):
+    def phase(self): 
         if not self.temporal: raise il.NotTemporalVariable()
-        tmax = il.MaxMonthMode(self.time,self.data)
+        if self.spatial is False:
+            tmax = il.MaxMonthMode(self.time,self.data)
+        else:
+            tmax = np.ma.masked_array(np.zeros((self.lat.size,self.lon.size)))
         return Variable(tmax,"d",name="day_of_max_%s" % self.name,lat=self.lat,lon=self.lon,area=self.area)
         
     def corrcoef(self,var,region="global"):

src/ILAMB/ilamblib.py
@@ -18,6 +18,9 @@ class VarNotInModel(Exception):
 class VarsNotComparable(Exception):
     pass
 
+class VarNotOnTimeScale(Exception):
+    pass
+
 class UnknownUnit(Exception):
     pass
 
@@ -925,8 +928,10 @@ def MaxMonthMode(t,var):
 
     Notes
     -----
-    This routine can be generalized to extract any kind of cycle information. Perhaps an
-    option for the user to specify a cycle time and we compute the rest from that.
+    This routine can be generalized to extract any kind of cycle
+    information. Perhaps an option for the user to specify a cycle
+    time and we compute the rest from that. Also, it appears to be
+    quite slow. It could be the mode function from scipy.mstats
     """
     assert t.ndim == 1
     assert t.size == var.shape[0]