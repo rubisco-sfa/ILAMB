137be536ef7ce7592c75bc00b74aebe4220beefd
Nathan Collier <nathaniel.collier@gmail.com>
2015-09-28 14:34:46 -0400

    added some disables to pass Chrome's new security requirements

--- src/ILAMB/Post.py
+++ src/ILAMB/Post.py
@@ -374,6 +374,13 @@ class HtmlLayout():
         code += """
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
     <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script>
+      $(document).bind('mobileinit',function(){
+        $.mobile.changePage.defaults.changeHash = false;
+        $.mobile.hashListeningEnabled = false;
+        $.mobile.pushStateEnabled = false;
+      });
+    </script>
     <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>"""
 
         # Add Google table of metrics
