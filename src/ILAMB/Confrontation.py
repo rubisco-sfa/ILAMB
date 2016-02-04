9c99ca1beefa8d6c6369385d883554c4de5699ce
Nathan Collier <nathaniel.collier@gmail.com>
2016-02-04 07:51:19 -0500

    forgot to possibly close benchmark dataset if analysis fails

--- src/ILAMB/Confrontation.py
+++ src/ILAMB/Confrontation.py
@@ -230,6 +230,9 @@ class Confrontation:
         except:
             results.close()
             os.system("rm -f %s/%s_%s.nc" % (self.output_path,self.name,m.name))
+            if self.master and benchmark_results is not None:
+                benchmark_results.close()
+                os.system("rm -f %s" % fname)
             raise il.AnalysisError()
         
         # Perform relationship analysis
