c3bdb2ff787386a57588fd0ff87a74eea59d1ac3
Nathan Collier <nathaniel.collier@gmail.com>
2017-02-20 18:11:35 -0500

    improvements to setup file for compliance with PyPI standards

--- setup.py
+++ setup.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python
-"""
-The International Land Model Benchmarking Project (ILAMB)
-"""
+from setuptools import setup
+from codecs import open
+from os import path
 
-NAME    = 'ILAMB'
-VERSION = '2.0.2'
-AUTHOR  = 'Nathan Collier'
-EMAIL   = 'nathaniel.collier@gmail.com'
-DESCR   = __doc__.strip()
-URL     = ''
-DLURL   = URL + '/get/default.tar.gz'
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-def setup_package():
-    from numpy.distutils.core import setup
-    from numpy.distutils.core import Extension
-    setup(name=NAME, version=VERSION,
-          author=AUTHOR, author_email=EMAIL,
-          description=DESCR, long_description=DESCR,
-          url=URL, download_url=DLURL,
-          packages = ['ILAMB'],
-          package_dir = {'ILAMB' : 'src/ILAMB'},
-          scripts=['bin/ilamb-run']
-          )
-
-if __name__ == "__main__":
-    setup_package()
+setup(
+    name='ILAMB',
+    version='2.0.2',
+    description='The International Land Model Benchmarking Package',
+    long_description=long_description,
+    url='https://bitbucket.org/ncollier/ilamb',
+    author='Nathan Collier',
+    author_email='nathaniel.collier@gmail.com',
+    #license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        #'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='benchmarking, earth system modeling',
+    packages=['ILAMB'],
+    package_dir={'ILAMB' : 'src/ILAMB'},
+    scripts=['bin/ilamb-run'],
+    install_requires=['numpy>=1.9.2',
+                      'matplotlib>=1.4.3',
+                      'netCDF4>=1.1.4',
+                      'cfunits>=1.1.4',
+                      'basemap>=1.0.7',
+                      'sympy>=0.7.6']
+)
