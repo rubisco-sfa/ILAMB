79474347c02c650be1c132bc4e730afafa369a4c
Nathan Collier <nathaniel.collier@gmail.com>
2017-06-08 11:53:11 -0400

    altered EvapFrac to mask out where not valid

--- src/ILAMB/ConfEvapFraction.py
+++ src/ILAMB/ConfEvapFraction.py
@@ -10,33 +10,49 @@ class ConfEvapFraction(Confrontation):
 
     def stageData(self,m):
 
-        obs = Variable(filename       = self.source,
-                       variable_name  = self.variable,
-                       alternate_vars = self.alternate_vars)
+        energy_threshold = float(self.keywords.get("energy_threshold",20.))
+        sh  = Variable(filename       = os.environ["ILAMB_ROOT"] + "/DATA/sh/GBAF/sh_0.5x0.5.nc",
+                       variable_name  = "sh")
+        le  = Variable(filename       = os.environ["ILAMB_ROOT"] + "/DATA/le/GBAF/le_0.5x0.5.nc",
+                       variable_name  = "le")
+        obs = Variable(name      = self.variable,
+                       unit      = "1",
+                       data      = np.ma.masked_array(le.data/(le.data+sh.data),
+                                                      mask=((le.data<0)+
+                                                            (sh.data<0)+
+                                                            ((le.data+sh.data)<energy_threshold))),
+                       lat       = sh.lat,
+                       lat_bnds  = sh.lat_bnds,
+                       lon       = sh.lon,
+                       lon_bnds  = sh.lon_bnds,
+                       time      = sh.time,
+                       time_bnds = sh.time_bnds)
+        
         if obs.time is None: raise il.NotTemporalVariable()
         self.pruneRegions(obs)
         
-        sh = m.extractTimeSeries("hfss",
-                                 initial_time = obs.time_bnds[ 0,0],
-                                 final_time   = obs.time_bnds[-1,1],
-                                 lats         = None if obs.spatial else obs.lat,
-                                 lons         = None if obs.spatial else obs.lon)
-        le = m.extractTimeSeries("hfls",
-                                 initial_time = obs.time_bnds[ 0,0],
-                                 final_time   = obs.time_bnds[-1,1],
-                                 lats         = None if obs.spatial else obs.lat,
-                                 lons         = None if obs.spatial else obs.lon)
-        sh.data = np.abs(sh.data)
-        le.data = np.abs(le.data)
-        mod     = Variable(name      = self.variable,
-                           unit      = "1",
-                           data      = le.data/(le.data+sh.data),
-                           lat       = sh.lat,
-                           lat_bnds  = sh.lat_bnds,
-                           lon       = sh.lon,
-                           lon_bnds  = sh.lon_bnds,
-                           time      = sh.time,
-                           time_bnds = sh.time_bnds)
+        sh  = m.extractTimeSeries("hfss",
+                                  initial_time = obs.time_bnds[ 0,0],
+                                  final_time   = obs.time_bnds[-1,1],
+                                  lats         = None if obs.spatial else obs.lat,
+                                  lons         = None if obs.spatial else obs.lon)
+        le  = m.extractTimeSeries("hfls",
+                                  initial_time = obs.time_bnds[ 0,0],
+                                  final_time   = obs.time_bnds[-1,1],
+                                  lats         = None if obs.spatial else obs.lat,
+                                  lons         = None if obs.spatial else obs.lon)
+        mod = Variable(name      = self.variable,
+                       unit      = "1",
+                       data      = np.ma.masked_array(le.data/(le.data+sh.data),
+                                                      mask=((le.data<0)+
+                                                            (sh.data<0)+
+                                                            ((le.data+sh.data)<energy_threshold))),
+                       lat       = sh.lat,
+                       lat_bnds  = sh.lat_bnds,
+                       lon       = sh.lon,
+                       lon_bnds  = sh.lon_bnds,
+                       time      = sh.time,
+                       time_bnds = sh.time_bnds)
         
         obs,mod = il.MakeComparable(obs,mod,
                                     mask_ref  = True,
@@ -46,3 +62,4 @@ class ConfEvapFraction(Confrontation):
         return obs,mod
     
 
+# 7.75 51.25
