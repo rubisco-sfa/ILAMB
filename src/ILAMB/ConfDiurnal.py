d07b37470f68ec9a905f45bcc2663c3f5a462dbe
Nathan Collier <nathaniel.collier@gmail.com>
2018-06-05 11:40:13 -1000

    major shift in methodology to resemble the NDVI seasonality work

--- src/ILAMB/ConfDiurnal.py
+++ src/ILAMB/ConfDiurnal.py
@@ -9,19 +9,79 @@ from netCDF4 import Dataset
 import ILAMB.ilamblib as il
 import numpy as np
 import os,glob
+from constants import lbl_months,bnd_months
 
-def DiurnalReshape(var):
-    dt    = (var.time_bnds[:,1]-var.time_bnds[:,0]).mean()
+def DiurnalReshape(time,time_bnds,data):
+    dt    = (time_bnds[:,1]-time_bnds[:,0])[:-1]
+    dt    = dt.mean()
     spd   = int(round(1./dt))
-    begin = np.argmin(var.time[:(spd-1)]%spd)
-    end   = begin+int(var.time[begin:].size/float(spd))*spd
-    shp   = (-1,spd) + var.data.shape[1:]
-    cycle = var.data[begin:end].reshape(shp)
-    tbnd  = var.time_bnds[begin:end,:].reshape((-1,spd,2)) % 1
-    tbnd  = tbnd[0,...]
-    tbnd[-1,1] = 1.
-    t     = tbnd.mean(axis=1)
-    return cycle,t,tbnd
+    begin = np.argmin(time[:(spd-1)]%spd)
+    end   = begin+int(time[begin:].size/float(spd))*spd
+    shp   = (-1,spd) + data.shape[1:]
+    cycle = data[begin:end].reshape(shp)
+    t     = time[begin:end].reshape(shp).mean(axis=1)
+    return cycle,t
+
+def _findSeasonalTiming(t,x):
+    """Return the beginning and ending time of the season of x.
+
+    The data x is assumed to start out relatively small, pass through
+    a seasonal period of high values, and then return to small values,
+    similar to a bell curve. This routine returns the times where
+    these seasonal changes occur. To do this, we accumulate the
+    variable x and then try to fit 3 linear segments to each seasonal
+    portion. We pose a problem then that finds the breaks which
+    minimizes the residual of these three best fit lines.
+
+    Parameters
+    ----------
+    time: numpy.ndarray
+        time array
+    x: numpy.ndarray
+        a single cycle of data to extract the season from
+
+    Returns
+    -------
+    tbnds: numpy.ndarray
+        the beginning and ending time in an array of size 2
+    """
+    def cost(t,y):
+        from scipy.stats import linregress
+        out = linregress(t,y)
+        return np.sqrt((((out.slope*t+out.intercept)-y)**2).sum())
+    y = x.cumsum()
+    b = y.size/2-1
+    e = y.size/2+1
+    I = np.asarray(range(2,b))
+    C = np.zeros(I.shape)
+    for a,i in enumerate(I): C[a] = cost(t[:i],y[:i]) + cost(t[i:e],y[i:e])
+    b = I[C.argmin()]
+    I = np.asarray(range(e,y.size-2))
+    C = np.zeros(I.shape)
+    for a,i in enumerate(I): C[a] = cost(t[b:i],y[b:i]) + cost(t[i:],y[i:])
+    e = I[C.argmin()]
+    return t[[b,e]]
+
+def _findSeasonalCentroid(t,x):
+    """Return the centroid of the season in polar and cartesian coordinates.
+
+    Parameters
+    ----------
+    time: numpy.ndarray
+        time array but scaled [0,2 pi]
+    x: numpy.ndarray
+        a single cycle of data to extract the season from
+
+    Returns
+    -------
+    centroid: numpy.ndarray
+        array of size 4, [r,theta,x,y]
+    """    
+    x0 = (x*np.cos(t/365.*2*np.pi)).mean()
+    y0 = (x*np.sin(t/365.*2*np.pi)).mean()
+    r0 = np.sqrt(x0*x0+y0*y0)
+    a0 = np.arctan2(y0,x0)
+    return r0,a0,x0,y0
 
 class ConfDiurnal(Confrontation):
     """A confrontation for examining the diurnal 
@@ -66,15 +126,15 @@ class ConfDiurnal(Confrontation):
                                   final_time   = obs.time_bnds[-1,1],
                                   lats         = None if obs.spatial else obs.lat,
                                   lons         = None if obs.spatial else obs.lon).convert(obs.unit)
-
+        
         # When we make things comparable, sites can get pruned, we
         # also need to prune the site labels
         lat = np.copy(obs.lat); lon = np.copy(obs.lon)
-        obs,mod = il.MakeComparable(obs,mod,clip_ref=True,prune_sites=True)
+        obs,mod = il.MakeComparable(obs,mod,clip_ref=True,prune_sites=True,allow_diff_times=True)
         ind = np.sqrt((lat[:,np.newaxis]-obs.lat)**2 +
                       (lon[:,np.newaxis]-obs.lon)**2).argmin(axis=0)
         maxS = max([len(s) for s in self.lbls])
-        self.lbls = np.asarray(self.lbls,dtype='S%d' % maxS)[ind]
+        self.lbls = np.asarray(self.lbls,dtype='S%d' % maxS)[ind]        
         return obs,mod
 
     def confront(self,m):
@@ -83,112 +143,102 @@ class ConfDiurnal(Confrontation):
         page = [page for page in self.layout.pages if "MeanState" in page.name][0]
         
         # Grab the data
-        obs,mod      = self.stageData(m)    
-        odata,ot,otb = DiurnalReshape(obs)
-        mdata,mt,mtb = DiurnalReshape(mod)
-
-        n            = self.lbls.size
-        obs_amp      = np.zeros(n)
-        mod_amp      = np.zeros(n)
-        amp_score    = np.zeros(n)
-        obs_phase    = np.zeros(n)
-        mod_phase    = np.zeros(n)
-        phase_score  = np.zeros(n)
-        for site in range(n):
-
-            # Site name
-            lbl   = self.lbls[site]
-            skip  = False
-            
-            # Observational diurnal cycle
-            tobs  = ot + obs.lon[site]/360
-            vobs  = odata[...,site]
-            vobs  = np.roll(vobs,-tobs.searchsorted(0),axis=1)
-            tobs  = np.roll(tobs,-tobs.searchsorted(0))
-            tobs += (tobs<0)
-            aobs  = (vobs.max(axis=1)-vobs.min(axis=1)).mean()
-            vobs  = vobs.mean(axis=0)
-            if vobs.size == vobs.mask.sum(): skip = True
-            if not skip:
-                acyc  = CubicSpline(np.hstack([tobs,tobs[0]+1.]),
-                                    np.hstack([vobs,vobs[0]   ]),
-                                    bc_type="periodic")
-                troot = acyc.derivative().solve()
-                troot = troot[(troot>=0)*(troot<=1.)]
-                otmx  = troot[acyc(troot).argmax()]
+        obs,mod = self.stageData(m)
+        Nobs = 365./np.diff(obs.time).mean()
+        Nmod = 365./np.diff(mod.time).mean()
         
-            # Model diurnal cycle
-            tmod  = mt + mod.lon[site]/360
-            vmod  = mdata[...,site]
-            vmod  = np.roll(vmod,-tmod.searchsorted(0),axis=1)
-            tmod  = np.roll(tmod,-tmod.searchsorted(0))
-            tmod += (tmod<0)
-            amod  = (vmod.max(axis=1)-vmod.min(axis=1)).mean()
-            vmod  = vmod.mean(axis=0)
-            mcyc  = CubicSpline(np.hstack([tmod,tmod[0]+1.]),
-                                np.hstack([vmod,vmod[0]   ]),
-                                bc_type="periodic")
-            troot = mcyc.derivative().solve()
-            troot = troot[(troot>=0)*(troot<=1.)]
-            mtmx  = troot[mcyc(troot).argmax()]
+        # Analysis on a per year basis
+        Yobs = (obs.time/365.+1850).astype(int)
+        Ymod = (mod.time/365.+1850).astype(int)
+        Y    = np.unique(Yobs)
+        S1 = []; S2 = []; S3 = []; Lobs = []; Lmod = []
+        Sobs = {}; Smod = {}
+        for y in Y:
 
-            # Scalars and scores
-            if skip:
-                obs_amp    [site] = np.nan
-                obs_phase  [site] = np.nan
-                amp_score  [site] = np.nan
-                phase_score[site] = np.nan
-            else:
-                obs_amp    [site] = aobs
-                obs_phase  [site] = otmx
-                amp_score  [site] = np.exp(-np.abs(amod-aobs)/aobs)
-                s = np.abs(mtmx-otmx)
-                s = (s<=0.5)*s + (s>0.5)*(1.-s)
-                s = 1.-s/0.5
-                phase_score[site] = s
+            # Subset the data
+            iobs = np.where(y==Yobs)[0]
+            imod = np.where(y==Ymod)[0]
+            if iobs.size/Nobs < 0.9: continue
+            if imod.size/Nmod < 0.9: continue
 
-            mod_amp    [site] = amod
-            mod_phase  [site] = mtmx
-
-            # Plot
-            ts     = np.linspace(0,1,100)
-            fig,ax = plt.subplots(figsize=(6.8,2.8),tight_layout=True)
-            if not skip:
-                ax.plot(tobs,vobs,'o',mew=0,markersize=3,color='k')
-                ax.plot(ts,acyc(ts),'-',color='k')
-                ax.plot(otmx,acyc(otmx),'o',mew=0,markersize=5,color='k')
-            ax.plot(tmod,vmod,'o',mew=0,markersize=3,color=m.color)
-            ax.plot(ts,mcyc(ts),'-',color=m.color)
-            ax.plot(mtmx,mcyc(mtmx),'o',mew=0,markersize=5,color=m.color)
-            xt  = np.arange(25)[::3]
-            xtl = ["%02d:00" % xx for xx in xt]
-            ax.set_xticks     (xt/24.)
-            ax.set_xticklabels(xtl   )
-            ax.grid(True)
-            ax.set_xlabel("Mean solar time")
-            ax.set_ylabel("[%s]" % obs.unit)
-            plt.savefig(os.path.join(self.output_path,"%s_diurnal_%s.png" % (m.name,lbl)))
-            plt.close()
-
-            obs_amp     = np.ma.masked_invalid(obs_amp)
-            obs_phase   = np.ma.masked_invalid(obs_phase)
-            amp_score   = np.ma.masked_invalid(amp_score)
-            phase_score = np.ma.masked_invalid(phase_score)
+            # Compute the diurnal magnitude
+            vobs,tobs = DiurnalReshape(obs.time     [iobs] % 365,
+                                       obs.time_bnds[iobs] % 365,
+                                       obs.data     [iobs,0])
+            vmod,tmod = DiurnalReshape(mod.time     [imod] % 365,
+                                       mod.time_bnds[imod] % 365,
+                                       mod.data     [imod,0])
+            vobs  = vobs.max(axis=1)-vobs.min(axis=1)
+            vmod  = vmod.max(axis=1)-vmod.min(axis=1)
+            Sobs[y] = Variable(name = "season_%d" % y,
+                               unit = obs.unit,
+                               time = tobs,
+                               data = vobs)
+            Smod[y] = Variable(name = "season_%d" % y,
+                               unit = mod.unit,
+                               time = tmod,
+                               data = vmod)
             
-            results = Dataset(os.path.join(self.output_path,"%s_%s.nc" % (self.name,m.name)),mode="w")
+            # Compute metrics
+            To  = _findSeasonalTiming  (tobs,vobs)
+            Ro  = _findSeasonalCentroid(tobs,vobs)
+            Tm  = _findSeasonalTiming  (tmod,vmod)
+            Rm  = _findSeasonalCentroid(tmod,vmod)
+            dTo = To[1]-To[0]       # season length of the observation
+            a   = np.log(0.1) / 0.5 # 50% relative error equals a score of 1/10
+            s1  = np.exp(a* np.abs(To[0]-Tm[0])/dTo)
+            s2  = np.exp(a* np.abs(To[1]-Tm[1])/dTo)
+            s3  = np.linalg.norm(np.asarray([Ro[2]-Rm[2],Ro[3]-Rm[3]])) #  |Ro - Rm|
+            s3 /= np.linalg.norm(np.asarray([      Ro[2],      Ro[3]])) # /|Ro|
+            s3  = np.exp(-s3)
+            S1.append(s1); S2.append(s2); S3.append(s3)
+            Lobs.append(To[1]-To[0])
+            Lmod.append(Tm[1]-Tm[0])
+
+        # Score by mean values across years
+        S1   = np.asarray(S1  ).mean()
+        S2   = np.asarray(S2  ).mean()
+        S3   = np.asarray(S3  ).mean()
+        Lobs = np.asarray(Lobs).mean()
+        Lmod = np.asarray(Lmod).mean()
+
+        with Dataset(os.path.join(self.output_path,"%s_%s.nc" % (self.name,m.name)),mode="w") as results:
             results.setncatts({"name" :m.name, "color":m.color})
-            Variable(name="Amplitude global"      ,unit=obs.unit,data=   mod_amp  .mean()).toNetCDF4(results,group="MeanState")
-            Variable(name="Max time global"       ,unit="h"     ,data=24*mod_phase.mean()).toNetCDF4(results,group="MeanState")
-            Variable(name="Amplitude Score global",unit="1"     ,data=   amp_score.mean()).toNetCDF4(results,group="MeanState")
-            Variable(name="Phase Score global"    ,unit="1"     ,data= phase_score.mean()).toNetCDF4(results,group="MeanState")
-            results.close()
-            if self.master:
-                results = Dataset(os.path.join(self.output_path,"%s_Benchmark.nc" % self.name),mode="w")
-                results.setncatts({"name" :"Benchmark", "color":np.asarray([0.5,0.5,0.5])})
-                Variable(name="Amplitude global"      ,unit=obs.unit,data=   obs_amp  .mean()).toNetCDF4(results,group="MeanState")
-                Variable(name="Max time global"       ,unit="h"     ,data=24*obs_phase.mean()).toNetCDF4(results,group="MeanState")
-                results.close()
-                
+            Variable(name = "Season Length global",
+                     unit = "d",
+                     data = Lmod).toNetCDF4(results,group="MeanState")
+            Variable(name = "Season Beginning Score global",
+                     unit = "1",
+                     data = S1).toNetCDF4(results,group="MeanState")
+            Variable(name = "Season Ending Score global",
+                     unit = "1",
+                     data = S2).toNetCDF4(results,group="MeanState")
+            Variable(name = "Season Strength Score global",
+                     unit = "1",
+                     data = S3).toNetCDF4(results,group="MeanState")
+            for key in Smod.keys(): Smod[key].toNetCDF4(results,group="MeanState")
+        if not self.master: return
+        with Dataset(os.path.join(self.output_path,"%s_Benchmark.nc" % self.name),mode="w") as results:
+            results.setncatts({"name" :"Benchmark", "color":np.asarray([0.5,0.5,0.5])})
+            Variable(name = "Season Length global",
+                     unit = "d",
+                     data = Lobs).toNetCDF4(results,group="MeanState")
+            for key in Sobs.keys(): Sobs[key].toNetCDF4(results,group="MeanState")
+
+    def determinePlotLimits(self):
+
+        self.limits = {}
+        self.limits["season"] = 0.
+        for fname in glob.glob(os.path.join(self.output_path,"*.nc")):
+            with Dataset(fname) as dataset:
+                if "MeanState" not in dataset.groups: continue
+                group     = dataset.groups["MeanState"]
+                variables = [v for v in group.variables.keys() if v not in group.dimensions.keys()]
+                for vname in variables:
+                    if "season" in vname:
+                        self.limits["season"] = max(self.limits["season"],group.variables[vname].up99)
+                        
+                    
     def modelPlots(self,m):
         
         bname  = "%s/%s_Benchmark.nc" % (self.output_path,self.name)
@@ -198,15 +248,26 @@ class ConfDiurnal(Confrontation):
 
         # get the HTML page
         page = [page for page in self.layout.pages if "MeanState" in page.name][0]
-        page.priority = ["Amplitude","Max","Min","Max time","Bias","RMSE","Shift","Score","Overall"]
+        page.priority = ["Beginning","Ending","Strength","Score","Overall"]
 
-        for site in range(self.lbls.size):
+        plots = []
+        with Dataset(bname) as dset:
+            plots = [key for key in dset.groups["MeanState"].variables.keys() if "season" in key]
+        plots.sort()
 
-            # Site name
-            lbl   = self.lbls[site]
+        for plot in plots:
+            obs = Variable(filename = bname, variable_name = plot, groupname = "MeanState")
+            mod = Variable(filename = fname, variable_name = plot, groupname = "MeanState")
+        
             page.addFigure("Diurnal cycle",
-                           lbl,
-                           "MNAME_diurnal_%s.png" % lbl,
-                           side   = lbl,
+                           plot,
+                           "MNAME_%s.png" % plot,
+                           side   = plot.split("_")[-1],
                            legend = False)
-
+            plt.figure(figsize=(5,5),tight_layout=True)
+            plt.polar(obs.time/365.*2*np.pi,obs.data,'-k',alpha=0.75)
+            plt.polar(mod.time/365.*2*np.pi,mod.data,'-',color=m.color)
+            plt.xticks(bnd_months[:-1]/365.*2*np.pi,lbl_months)
+            plt.ylim(0,self.limits["season"])
+            plt.savefig("%s/%s_%s.png" % (self.output_path,m.name,plot))
+            plt.close()
