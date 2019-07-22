df3e3b868f2c59a53b3d4cf3f34a712fbf6e00ec
Nathan Collier <nathaniel.collier@gmail.com>
2019-07-22 15:53:25 -0400

    Added some initial scalars

--- src/ILAMB/ConfSoilCarbon.py
+++ src/ILAMB/ConfSoilCarbon.py
@@ -1,8 +1,13 @@
 from ILAMB.Confrontation import Confrontation
 from ILAMB.Variable import Variable
 import matplotlib.pyplot as plt
+from netCDF4 import Dataset
 import numpy as np
 import os
+
+"""
+* 
+"""
 
 def getSource(filename,unit):
     vname = filename.split("/")[-1].split("_")[0]
@@ -23,17 +28,44 @@ def getSource(filename,unit):
             data[ind] = v.data[ind]
     return lat,lon,data
 
+def SensitivityPlot(tas,tau,pr,X,Y,std,gauss_critval,filename):
+    fig,ax = plt.subplots(figsize=(5,5.5),tight_layout=True,dpi=200)
+    sc = ax.scatter(tas,tau,c=pr,s=0.1,alpha=1,vmin=0,vmax=2000,cmap='wetdry')
+    ax.semilogy(X,Y,'-r',lw=2)
+    ax.semilogy(X,10**(np.log10(Y)-gauss_critval*std),'-r',lw=1)
+    ax.semilogy(X,10**(np.log10(Y)+gauss_critval*std),'-r',lw=1)
+    ax.set_yscale('log')
+    ax.set_xlim(-22,30)
+    ax.set_ylim(1,3e3)
+    ax.set_xlabel("Mean air temperature [$^{\circ}$C]")
+    ax.set_ylabel("Inferred turnover time [yr]")
+    fig.colorbar(sc,orientation='horizontal',pad=0.15,label='Precipitation [mm yr$^{-1}$]')
+    plt.savefig(filename)
+    plt.close()
+    
 class ConfSoilCarbon(Confrontation):
     """
     """
+    def __init__(self,**keywords):
+        super(ConfSoilCarbon,self).__init__(**keywords)
+        self.regions        = ["global"]
+        self.layout.regions = self.regions
+    
     def stageData(self,m):
+        pass
+    
+    def confront(self,m):
 
         # Constants
-        small_npp         =  1e-4  # kg m-2 yr-1
+        soilc_threshold   =  1e-12 # kg m-2
+        npp_threshold     =  1e-4  # kg m-2 yr-1
         aridity_threshold = -1000  # mm yr-1
         sat_threshold     =  0.5   # 1
         gauss_critval     =  0.674 # for 50%
-        
+        relationship_bins = self.keywords.get("relationship_bins",np.arange(-15.5,28.6,1))
+        if type(relationship_bins) == str:
+            relationship_bins = np.asarray(relationship_bins.split(","),dtype=float)
+            
         # Get the source datafiles
         lat,lon,soilc = getSource(self.keywords.get("soilc_source"),"kg m-2")
         LAT,LON,npp = getSource(self.keywords.get("npp_source"),"kg m-2 yr-1")
@@ -49,48 +81,22 @@ class ConfSoilCarbon(Confrontation):
 
         # Determine what will be masked
         mask  = soilc.mask + tas.mask + npp.mask + pr.mask  # where any source is masked
-        mask += (soilc < 1e-12)  # where there is no soilc
-        mask += (npp < small_npp)  # where npp is small or negative
+        mask += (soilc < soilc_threshold)  # where there is no soilc
+        mask += (npp < npp_threshold)  # where npp is small or negative
         mask += ((pr-pet) < aridity_threshold)  # where aridity dominates
         mask += (fracpeat > sat_threshold)  # where mostly peatland
         soilc = np.ma.masked_array(soilc,mask=mask).compressed()
         npp   = np.ma.masked_array(npp  ,mask=mask).compressed()
         tas   = np.ma.masked_array(tas  ,mask=mask).compressed()
         pr    = np.ma.masked_array(pr   ,mask=mask).compressed()
-        
+
         # Compute inferred turnover and fit quadratic
         tau = soilc/npp
         p   = np.polyfit(tas,np.log10(tau),2)
         std = np.sqrt(((np.log10(tau) - np.polyval(p,tas))**2).sum()/tas.size)
         X   = np.linspace(-22,30,100)
         Y   = 10**np.polyval(p,X)
-
-        # Plot for checking
-        fig,ax = plt.subplots(figsize=(9,8),nrows=2,ncols=2,tight_layout=True)
-        with np.errstate(under='ignore'):
-            ax[0,0].scatter(tas,soilc,color='k',s=0.1,alpha=1)
-            ax[0,1].scatter(tas,npp  ,color='k',s=0.1,alpha=1)
-            ax[1,0].scatter(tas,tau  ,c    = pr,s=0.1,alpha=1,vmin=0,vmax=2000,cmap='wetdry')
-            ax[1,1].scatter(tas,tau  ,color='k',s=0.1,alpha=1)
-            ax[1,1].semilogy(X,Y,'-r',lw=2)
-            ax[1,1].semilogy(X,10**(np.log10(Y)-gauss_critval*std),'-r',lw=1)
-            ax[1,1].semilogy(X,10**(np.log10(Y)+gauss_critval*std),'-r',lw=1)
-            for i in range(2):
-                for j in range(2):
-                    ax[i,j].set_yscale('log')
-                    ax[i,j].set_xlim(-22,30)
-                    ax[i,j].set_xlabel("Mean air temperature [$^{\circ}$C]")
-        ax[0,0].set_ylim(0.1,300)
-        ax[0,1].set_ylim(1e-3,3)
-        ax[1,0].set_ylim(1,3e3)
-        ax[1,1].set_ylim(1,3e3)
-        ax[0,0].set_ylabel("NCSCD/HWSD Soil carbon to 1 m [kg m$^{-2}$]")
-        ax[0,1].set_ylabel("MODIS npp [kg m$^{-2}$ yr$^{-1}$]")
-        ax[1,0].set_ylabel("Inferred turnover time [yr]")
-        ax[1,1].set_ylabel("Inferred turnover time [yr]")
-        plt.savefig("fig1.pdf")
-        plt.close()
-
+        
         # Get model results
         y0 = self.keywords.get("y0",1980.)
         yf = self.keywords.get("yf",2006.)
@@ -111,8 +117,64 @@ class ConfSoilCarbon(Confrontation):
                                         initial_time = t0,
                                         final_time   = tf).integrateInTime(mean=True).convert("mm yr-1")
 
-        return None,None
-    
+        # Determine what will be masked
+        mask  = mod_soilc.data.mask + mod_npp.data.mask + mod_tas.data.mask + mod_pr.data.mask
+        mask += (mod_soilc.data < soilc_threshold)
+        mask += (mod_npp.data < npp_threshold)
+        mod_soilc = np.ma.masked_array(mod_soilc.data,mask=mask).compressed()
+        mod_npp   = np.ma.masked_array(mod_npp.data  ,mask=mask).compressed()
+        mod_tas   = np.ma.masked_array(mod_tas.data  ,mask=mask).compressed()
+        mod_pr    = np.ma.masked_array(mod_pr.data   ,mask=mask).compressed()
+
+        # Compute inferred turnover and fit quadratic
+        mod_tau = mod_soilc/mod_npp
+        mod_p   = np.polyfit(mod_tas,np.log10(mod_tau),2)
+        mod_std = np.sqrt(((np.log10(mod_tau) - np.polyval(mod_p,mod_tas))**2).sum()/mod_tas.size)
+        mod_X   = np.linspace(-22,30,100)
+        mod_Y   = 10**np.polyval(p,X)
+
+        # Binned Relationship RMSE
+        mean_obs = np.ma.masked_array(np.zeros(relationship_bins.size-1),mask=True)
+        mean_mod = np.ma.masked_array(np.zeros(relationship_bins.size-1),mask=True)
+        bins = np.digitize(mod_tas,relationship_bins).clip(1,relationship_bins.size-1)-1
+        rmse = 0.
+        for i in np.unique(bins):
+            tmid = 0.5*(relationship_bins[i]+relationship_bins[i+1])
+            mean_obs[i] = np.polyval(p,tmid)
+            mean_mod[i] = np.log10(mod_tau[bins==i]).mean()
+            rmse += mean_obs[i]**2 + mean_mod[i]**2
+        rmse = np.sqrt(rmse)
+        
+        # Outputs and plots
+        page = [page for page in self.layout.pages if "MeanState" in page.name][0]
+        
+        if self.master:
+            page.addFigure("Temporally integrated period mean",
+                           "benchmark_timeint",
+                           "Benchmark_global_timeint.png",
+                           side   = "BENCHMARK",
+                           legend = False)
+            SensitivityPlot(tas,tau,pr,X,Y,std,gauss_critval,"%s/Benchmark_global_timeing.png" % (self.output_path))
+            with Dataset("%s/%s_Benchmark.nc" % (self.output_path,self.name),mode="w") as results:
+                results.setncatts({"name" :"Benchmark", "color":np.asarray([0.5,0.5,0.5])})
+                Variable(name = "T^2",unit="1",data=p[0]).toNetCDF4(results)
+                Variable(name = "T"  ,unit="1",data=p[1]).toNetCDF4(results)
+                Variable(name = "1"  ,unit="1",data=p[2]).toNetCDF4(results)
+                
+        page.addFigure("Temporally integrated period mean",
+                       "timeint",
+                       "MNAME_global_timeint.png",
+                       side   = "MODEL",
+                       legend = False)
+        SensitivityPlot(mod_tas,mod_tau,mod_pr,mod_X,mod_Y,mod_std,gauss_critval,
+                        "%s/%s_global_timeing.png" % (self.output_path,m.name))
+        with Dataset("%s/%s_%s.nc" % (self.output_path,self.name,m.name),mode="w") as results:
+            results.setncatts({"name" :m.name, "color":m.color})
+            Variable(name = "T^2" ,unit="1",data=mod_p[0]).toNetCDF4(results)
+            Variable(name = "T"   ,unit="1",data=mod_p[1]).toNetCDF4(results)
+            Variable(name = "1"   ,unit="1",data=mod_p[2]).toNetCDF4(results)
+            Variable(name = "RMSE",unit="1",data=rmse    ).toNetCDF4(results)
+
 if __name__ == "__main__":
     from ILAMB.ModelResult import ModelResult
     from ILAMB.Post import RegisterCustomColormaps
@@ -125,14 +187,4 @@ if __name__ == "__main__":
                        npp_source = "DATA/soilc/Koven/npp_0.5x0.5.nc",
                        pet_source = "DATA/soilc/Koven/pet_0.5x0.5.nc",
                        fracpeat_source = "DATA/soilc/Koven/fracpeat_0.5x0.5.nc")
-    obs,mod = c.stageData(m)
-
-
-
-
-
-
-
-    
-
-    
+    c.confront(m)
