52cd1ddd8e13348700582fa4ca08020f12b82d1f
Nathan Collier <nathaniel.collier@gmail.com>
2019-07-23 09:10:00 -0400

    added a duplicate global region so when global land is used, we can also do global all

--- src/ILAMB/Regions.py
+++ src/ILAMB/Regions.py
@@ -180,6 +180,8 @@ if "global" not in Regions().regions:
     r = Regions()
     r.addRegionLatLonBounds("global","Globe",(-89.999, 89.999),(-179.999, 179.999))
     Regions._regions["global"][3][...] = 0. # ensure global mask is null
+    r.addRegionLatLonBounds("globe","Global - All",(-89.999, 89.999),(-179.999, 179.999))
+    Regions._regions["globe"][3][...] = 0. # ensure global mask is null
 
     # GFED regions
     r.addRegionLatLonBounds("bona","Boreal North America",             ( 49.75, 79.75),(-170.25,- 60.25))
