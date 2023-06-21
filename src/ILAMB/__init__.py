922a988fea89cabd3f818611f8e6e2a0d946b04c
Nathan Collier <nathaniel.collier@gmail.com>
2023-06-21 11:31:54 -0400

    remove needless checks

--- src/ILAMB/__init__.py
+++ src/ILAMB/__init__.py
@@ -1,25 +1,3 @@
 __author__ = "Nathan Collier"
 __date__ = "May 2021"
 __version__ = "2.6"
-
-from distutils.version import LooseVersion
-import platform
-
-# These are guesses at actual requirements
-requires = {
-    "numpy": "1.9.2",
-    "matplotlib": "1.4.3",
-    "netCDF4": "1.1.4",
-    "cf_units": "2.0.0",
-    "cartopy": "0.17.0",
-    "sympy": "0.7.6",
-    "mpi4py": "1.3.1",
-}
-
-for key in requires.keys():
-    pkg = __import__(key)
-    if LooseVersion(pkg.__version__) < LooseVersion(requires[key]):
-        raise ImportError(
-            "Bad %s version: ILAMB %s requires %s >= %s got %s"
-            % (key, __version__, key, requires[key], pkg.__version__)
-        )
