ec7032caca34b767382cc5f689f6b691e6ee8f25
Nathan Collier <nathaniel.collier@gmail.com>
2023-06-28 07:41:52 -0400

    rename of README

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ if not release:
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 write_version_py()
