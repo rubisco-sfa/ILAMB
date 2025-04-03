5fa3c0320e37120382f51aac125ec36b21e2319b
Nathan Collier <nathaniel.collier@gmail.com>
2025-04-03 10:39:04 -0400

    don't enforce areas if no areas are present

--- src/ILAMB/ModelResult.py
+++ src/ILAMB/ModelResult.py
@@ -368,7 +368,9 @@ class ModelResult:
                 # hack to avoid an issue where an unstructured grid
                 # has an area and then when ILAMB interpolates it
                 # passes areas that are the wrong shape.
-                if not np.allclose(var.area.shape, [var.lat.size, var.lon.size]):
+                if var.area and not np.allclose(
+                    var.area.shape, [var.lat.size, var.lon.size]
+                ):
                     var.area = il.CellAreas(
                         None, None, lat_bnds=var.lat_bnds, lon_bnds=var.lon_bnds
                     )
